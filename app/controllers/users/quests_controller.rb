b0b01e62d87fc773beb93f32250547dc5182c937
Adam Etzion <adametzion90@gmail.com>
2021-02-26 12:59:26 -0700

    remove pry

--- app/controllers/users/quests_controller.rb
+++ app/controllers/users/quests_controller.rb
@@ -2,7 +2,6 @@ class Users::QuestsController < ApplicationController
 
   def create
     conn = Faraday.new(url: "https://chess-quest-api.herokuapp.com/api/v1/users/#{current_user.id}/")
-    require 'pry'; binding.pry
     response = conn.post('quests')
 
     data = JSON.parse(response.body, symbolize_names: true)
