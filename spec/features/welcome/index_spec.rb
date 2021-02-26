86c2e23481843799b746b27ad34f65808b00431c
Phil McCarthy <philmcc2631@gmail.com>
2021-02-25 17:54:05 -0700

    Add feature test for user login with OAuth mocked and stubbed
    Co-authored-by: Adam Etzion <60630167+aetzion1@users.noreply.github.com>

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -1,10 +1,48 @@
 require 'rails_helper'
 
-describe 'the app is up' do
-  it 'when I visit root' do
+describe 'Welcome Index', type: :feature do
+  it 'when I visit the root path I see a description and a link to sign in with Google' do
     visit root_path
-
+    
     expect(page).to have_content('Welcome')
     expect(page).to have_content('Sign In With Google')
+    # click_link 'Sign In With Google'
+    # expect(current_path).to be(user_google_oauth2_omniauth_authorize_path)
+  end
+
+  it 'logs the user in via google mock' do
+    user_count = User.count
+    expect(user_count).to eq(0)
+
+    stub_omniauth
+    user_count = User.count
+    expect(user_count).to eq(0)
+
+    visit root_path
+
+    click_link 'Sign In With Google'
+
+    user_count = User.count
+    expect(user_count).to eq(1)
+    user = User.first
+
+    expect(page).to have_content("Welcome to Chess Quest, #{user.email}")
+  end
+
+  it 'logs a returning user in via google mock' do
+    stub_omniauth
+    user = User.create(email: 'john@example.com', uid: '100000000000000000000')
+    user_count = User.count
+    expect(user_count).to eq(1)
+
+    visit root_path
+
+    click_link 'Sign In With Google'
+
+    user_count = User.count
+    expect(user_count).to eq(1)
+
+    expect(current_path).to eq(users_dashboard_path(user))
+    expect(page).to have_content("Welcome to Chess Quest, #{user.email}")
   end
 end
