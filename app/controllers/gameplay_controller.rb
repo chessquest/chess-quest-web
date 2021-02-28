0ef357292efc12d97447796dfaea1ea7c5b22c3e
Phil McCarthy <philmcc2631@gmail.com>
2021-02-27 18:22:05 -0700

    Fix route in gameplay_controller

--- app/controllers/gameplay_controller.rb
+++ app/controllers/gameplay_controller.rb
@@ -1,7 +1,7 @@
 class GameplayController < ApplicationController
   def show
-    conn = Faraday.new(url: "https://chess-quest-api.herokuapp.com/api/v1/users/#{current_user.id}/")
-    response = conn.get("games/#{params['game_id']}")
+    conn = Faraday.new(url: "https://chess-quest-api.herokuapp.com/api/v1/")
+    response = conn.get("#{params['game_id']}")
 
     data = JSON.parse(response.body, symbolize_names: true)
     @game = Game.new(data[:data])
