007152f4d75a8c4677d1c027ab917d11f1e7039a
Liam Cusack <LCusack8108@gmail.com>
2021-03-01 19:34:16 -0700

    Add game selection partial, add drop down selection for game creation
    Co-authored-by: Max Ribbans <52083805+ribbansmax@users.noreply.github.com>

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,9 @@
 class GamesController < ApplicationController
   def create
+    binding.pry
     conn = Faraday.new(url: "https://chess-quest-api.herokuapp.com/api/v1/users/#{current_user.id}/")
     response = conn.post('games') do |req|
-      req.params['find_player'] = 'magnuscarlsen'
+      req.params['find_player'] = params[:player]
     end
 
     data = JSON.parse(response.body, symbolize_names: true)
