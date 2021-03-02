0b4ec6b071b184769f5fe93c4a6976b6ba870dfc
Ely Hess <Ely.hess@me.com>
2021-03-01 19:03:42 -0700

    add @current_quest & @completed_quests
    Co-authored-by: Phil McCarthy <9437806+philmccarthy@users.noreply.github.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,20 +1,7 @@
 class DashboardController < ApplicationController
   def index
-    conn = Faraday.new(url: "https://chess-quest-api.herokuapp.com/api/v1/users/#{current_user.id}/")
-
-    response = conn.get('quests') do |req|
-      req.params[:status] = 'in_progress'
-    end
-    
-    data = JSON.parse(response.body, symbolize_names: true)
-    return @quests = nil if data[:data].blank?
-
-    # data = data[:data]
-
-    @quests = data[:data].map do |datum|
-      Quest.new(datum)
-    end
-
+    @current_quest = QuestsFacade.get_current_quest(current_user)
+    @completed_quests = QuestsFacade.get_completed_quests(current_user)
     @current_game = nil
   end
 end
