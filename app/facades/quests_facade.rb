2a5999e4f87faefc98bbfd263bde1b3182efc4e2
Max Ribbans <ribbans.max@gmail.com>
2021-03-03 19:00:34 -0500

    Continue quest button
    Co-authored-by: Adam Etzion <60630167+aetzion1@users.noreply.github.com>
    Co-authored-by: Ely Hess <46981974+elyhess@users.noreply.github.com>
    Co-authored-by: Phil McCarthy <9437806+philmccarthy@users.noreply.github.com>

--- app/facades/quests_facade.rb
+++ app/facades/quests_facade.rb
@@ -8,6 +8,13 @@ class QuestsFacade
 			end
 		end
 
+		def get_current_game(user)
+			data = QuestService.call_current_quest(user)
+			if data.present? && data[:data].present? 
+				dig_through_it(data)
+			end
+		end
+
 		def get_completed_quests(user)
 			data = QuestService.call_completed_quests(user)
 			if data.present? && data[:data].present?
@@ -25,6 +32,14 @@ class QuestsFacade
 			data[:data].map do |datum|
 				Quest.new(datum)
 			end
+		end
+
+		def dig_through_it(data)
+			return nil if !data[:included]
+			game = data[:included].select do |game|
+				game[:attributes][:status] == 'in_progress'
+			end.first
+			Game.new(game)
 		end
 	end
 end
